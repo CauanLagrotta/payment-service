1fe3ce256cc420bc91f043223b93315ff41d1b38
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-07-24 21:54:38 -0300

    feat(payment): update PaymentService interface with checked exceptions

--- src/main/java/com/cauanlagrotta/service/PaymentService.java
+++ src/main/java/com/cauanlagrotta/service/PaymentService.java
@@ -6,16 +6,18 @@ import com.cauanlagrotta.dto.UserDTO;
 import com.cauanlagrotta.model.PaymentOrder;
 import com.cauanlagrotta.payload.response.PaymentLinkResponse;
 import com.razorpay.PaymentLink;
+import com.razorpay.RazorpayException;
+import com.stripe.exception.StripeException;
 
 public interface PaymentService {
 
-  PaymentLinkResponse createOrder(UserDTO user, BookingDTO booking, PaymentMethod paymentMethod);
+  PaymentLinkResponse createOrder(UserDTO user, BookingDTO booking, PaymentMethod paymentMethod) throws RazorpayException, StripeException;
 
-  PaymentOrder getPaymentOrderById(Long id);
+  PaymentOrder getPaymentOrderById(Long paymentOrderId);
 
   PaymentOrder getPaymentOrderByPaymentId(String paymentId);
 
-  PaymentLink createRazorpayPaymentLink(UserDTO user, Long amount, Long orderId);
+  PaymentLink createRazorpayPaymentLink(UserDTO user, Long amount, Long orderId) throws RazorpayException;
 
-  String createStripePaymentLink(UserDTO user, Long amount, Long orderId);
+  String createStripePaymentLink(UserDTO user, Long amount, Long orderId) throws StripeException;
 }
